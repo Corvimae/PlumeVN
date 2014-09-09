3b9646b76721c7f223e7ba29408cc55be305b3f4
May Roussel <may@maybreak.com>
2014-09-09 14:35:00 -0500

    Various features, fixes
    Added UIGroup element, added support for choice blocks, added event
    handling (currently only click events), added bounding boxes.

--- test1.plume/scene1.scene/scriptTest.py
+++ test1.plume/scene1.scene/scriptTest.py
@@ -1,7 +1,7 @@
 import plume
 
 print "This runs on initial startup."
-plume.getElement("test_elem_3").setProperty("x", 500)
+plume.getElement("test_elem_3").setProperty("value", "I am a label!")
 
 print plume.getElement("test_elem_3").getProperty("visible")
 
@@ -18,3 +18,6 @@ def test2(data, num):
 	total = int(num) + 20
 	print data + " The number passed in plus 20 is " + str(total)
 	print plume.getCurrentLine() + " :("
+	
+def groupClickTest():
+	print "Clicked!"
