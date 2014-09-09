70cf63d0aadb517a91e9a4f2ac716d05cb04832e
Jake Roussel <jakeroussel@mac.com>
2014-09-09 14:35:00 -0500

    Various features, fixes
    Added UIGroup element, added support for choice blocks, added event
    handling (currently only click events), added bounding boxes.

--- js/plume.js
+++ js/plume.js
@@ -41,9 +41,13 @@ function GotoItem(target) {
 	this.target = target;
 }
 
+// ----------- START OF CLASS DEFINITIONS ----------- 
+
 function UIElement(id) {
 	this.class = "UIElement";
 	this.id = id;
+	this.boundingBox = null;
+	this.parent = null;
 	this.properties = {
 		x: 0,
 		y: 0,
@@ -63,19 +67,51 @@ function UIElement(id) {
 	this.getProperty = function(key) {
 		return this.properties[key];
 	}
+	
+	this.setProperty = function(key, value) {
+		this.properties[key] = value;
+	}
+	
+	this.hasEvents = function() {
+		return Object.size(this.events) > 0;
+	}
+	
+	this.recalculateBoundingBox = function() {
+		var ctx = Plume.prototype.instance.drawContext;
+		var basePos = this.getParentBasePosition();
+		this.boundingBox = {
+			x1: basePos.x + this.properties.x,
+			y1: basePos.y + this.properties.y - 14,
+			x2: basePos.x + this.properties.x + ctx.measureText(this.id).width,
+			y2: basePos.y + this.properties.y
+		}
+	}
+	
+	this.getParentBasePosition = function() {
+		var x = 0;
+		var y = 0;
+		var parent = this.parent;
+		while(parent) {
+			x += parent.properties.x;
+			y += parent.properties.y;
+			parent = parent.parent;
+		}
+		return {x: x, y: y};
+	}
 }
 
 function UIString(id) {
 	var base = new UIElement(id);
-	
+	base.class = "UIString";
+	var dx = 0, dy = 0;
 	base.draw = function(ctx) {
-		var dx = this.getProperty("x"), dy = this.getProperty("y");
+		dx = this.getProperty("x"), dy = this.getProperty("y");
 		ctx.font = this.getProperty("font") || "14px Helvetica";
 		ctx.fillStyle = this.getProperty("color");
 		//Draw character by character, applying line formatting.
 		var line = this.properties.value;
-		for(var i = 0; i < this.getProperty("value").length; i++) {
-			var nextChar = this.getProperty("value").substring(i, i + 1);
+		for(var i = 0; i < line.length; i++) {
+			var nextChar = line.substring(i, i + 1);
 			if(nextChar === "<" && line.substring(i - 1, i) !== "\\") {
 				//We're in a tag, so find the whole tag.
 				var endTagPosition = line.substring(i, line.length).search(/[^\\]>/);
@@ -102,12 +138,26 @@ function UIString(id) {
 		}
 	}
 	
+	base.recalculateBoundingBox = function() {	
+		var ctx = Plume.prototype.instance.drawContext;	
+		var basePos = this.getParentBasePosition();
+		ctx.font = this.getProperty("font") || "14px Helvetica"
+		//Get the font size
+		var fontSize = new RegExp("([0-9]+)[Pp][Xx]").exec(ctx.font)[1];
+		this.boundingBox =  {
+			x1: basePos.x + this.properties.x,
+			y1: basePos.y + this.properties.y - fontSize,
+			x2: basePos.x + this.properties.x + dx,
+			y2: basePos.y + dy
+		}
+	}
+	
 	return base;
 }
 
 function UIRect(id) {
 	var base = new UIElement(id);
-	
+	base.class = "UIRect";
 	base.draw = function(ctx) {
 		ctx.beginPath();
 		ctx.rect(this.getProperty('x'), this.getProperty('y'), this.getProperty('width'), this.getProperty('height'));
@@ -119,19 +169,100 @@ function UIRect(id) {
 		}
 		ctx.closePath();
 	}
+	
+	base.recalculateBoundingBox = function() {
+		var basePos = this.getParentBasePosition();
+		this.boundingBox =  {
+			x1: basePos.x + this.properties.x,
+			y1: basePos.y + this.properties.y,
+			x2: basePos.x + this.properties.x + parseInt(this.properties.width, 10),
+			y2: basePos.y + this.properties.y + parseInt(this.properties.height, 10)
+		}
+	}
 	return base;
 }
 
 function UIImage(id) {
 	var base = new UIElement(id);
+	base.class = "UIImage";
+	var img = document.getElementById("plume_image_" + this.getProperty("image"));
+	this.width = img.width;
+	this.height = img.height;
+	
 	base.draw = function(ctx) {
-		ctx.drawImage(document.getElementById("plume_image_" + this.getProperty("image")), this.getProperty("x"), this.getProperty("y"));
+		var img = document.getElementById("plume_image_" + this.getProperty("image"));
+		ctx.drawImage(img, this.getProperty("x"), this.getProperty("y"));
+	}
+	
+	base.recalculateBoundingBox = function() {
+		var basePos = this.getParentBasePosition();
+		this.boundingBox =  {
+			x1: basePos.x + this.properties.x,
+			y1: basePos.y + this.properties.y,
+			x2: basePos.x + this.properties.x + this.width,
+			y2: basePos.y + this.properties.y + this.height
+		}
 	}
 	return base;
 }
 
+function UIGroup(data) {
+	var base = new UIElement(data.id);
+	base.class = "UIGroup";
+	var elems = [];
+	var minX = 0, minY = 0, maxX = 0, maxY = 0;
+
+	for(i = 0; i < data.children.length; i++) {
+		var newElem = Plume.prototype.processElementFromDefinition(data.children[i]);
+		elems.push(newElem);
+		newElem.parent = base;
+		Plume.prototype.instance.elementLookupTable.push(newElem);
+	}
+	
+	base.draw = function(ctx) {
+		elems.sort(function(a, b) {
+			return a.properties.z - b.properties.z;
+		});
+		ctx.save();
+		ctx.translate(this.getProperty('x'), this.getProperty('y'));
+		for(var i = 0; i < elems.length; i++) {
+			var elem = elems[i];
+			ctx.save();
+			if(elem.properties.visible) Plume.prototype.instance.drawSpecificElement(ctx, elem);
+			ctx.restore();
+		}
+		ctx.restore();
+	}
+	
+	base.recalculateBoundingBox = function() {
+		var basePos = this.getParentBasePosition();
+		minX = this.properties.x, minY = this.properties.y, maxX = -99999, maxY = -99999
+		for(var i = 0; i < elems.length; i++) {
+			var elem = elems[i];
+			var box = elem.boundingBox;
+			if(box !== null) {
+				if(box.x2 > maxX) maxX = box.x2;
+				if(box.y2 > maxY) maxY = box.y2;
+				if(box.x1 < minX) minX = box.x1;
+				if(box.y1 < minY) minY = box.y1;
+			}
+		}
+		this.boundingBox = {
+			x1: minX,
+			y1: minY,
+			x2: maxX,
+			y2: maxY
+		}
+	}
+	return base;
+}
+
+// ----------- END OF CLASS DEFINITIONS ----------- 
+
+
 //Variables
 Plume.prototype.elements = [];
+Plume.prototype.elementLookupTable = [];
 Plume.prototype.waitTime = 0; //Delay for a wait element
 
 Plume.prototype.loadProject = function(file) {
@@ -172,23 +303,20 @@ Plume.prototype.loadInterface = function(file) {
 	
 	for(var i = 0; i < json.elements.length; i++) {
 		var elem = json.elements[i];
-		var newItem;
-		switch(elem.class) {
-			case "UIElement": 	newItem = new UIElement(elem.id); break;
-			case "UIString":	newItem = new UIString(elem.id); break;
-			case "UIRect":		newItem = new UIRect(elem.id); break;
-			case "UIImage":		newItem = new UIImage(elem.id); break;
-		}
-		newItem.properties = elem.properties;
-		newItem.events = elem.events;
+		var newItem = this.processElementFromDefinition(elem);
 		this.elements.push(newItem);
+		this.elementLookupTable.push(newItem);
 	}
 	//Search for main_text_display
-	var doesMainDisplayExist = false;
-	for(var i = 0; i < this.elements.length; i++) {
-		if(this.elements[i].id === "main_text_display") {
+	var doesMainDisplayExist = false, doesMainOptionDisplayExist = false;
+	for(var i = 0; i < this.elementLookupTable.length; i++) {
+		if(this.elementLookupTable[i].id === "main_text_display") {
 			doesMainDisplayExist = true;
-			this.mainDisplay = this.elements[i];
+			this.mainDisplay = this.elementLookupTable[i];
+		}
+		if(this.elementLookupTable[i].id === "main_option_display") {
+			doesMainOptionDisplayExist = true;
+			this.mainOptionDisplay = this.elementLookupTable[i];
 		}
 	}
 	if(!doesMainDisplayExist) {
@@ -202,11 +330,82 @@ Plume.prototype.loadInterface = function(file) {
 			"color": "rgb(0,0,0)",
 			"value": "Test"
 		};
-		mainDisplay.elements = {};
+		mainDisplay.events = {};
 		this.elements.push(mainDisplay);
+		this.elementLookupTable.push(mainDisplay);
 		this.mainDisplay = mainDisplay;
 	}
+	
+	if(!doesMainOptionDisplayExist) {
+		var canvas = document.getElementById('plumeCanvas');
+		var elementList = [];
+		for(var i = 0; i < 4; i++) {
+			var newOption = new UIString('main_option_display_selection_' + (3 - i));
+			newOption.properties = {
+				"x": 0,
+				"y": -40*i,
+				"z": 0,
+				"visible": true,
+				"color": "rgb(0,0,0)",
+				"value": "Option " + (3 - i)
+			}
+			newOption.events = {};
+			elementList.push(newOption);
+		}
+		var mainText = new UIString('main_option_display_dialog');
+		mainText.properties = {
+			"x": 0,
+			"y": -160,
+			"z": 0,
+			"visible": true,
+			"color": "rgb(0,0,0)",
+			"value": "Dialog"
+		}
+		mainText.events = {};
+		elementList.push(mainText);
+		var data = {
+			"id": "main_option_display",
+			"properties": {
+				"x": 50,
+				"y": canvas.height - 50,
+				"z": 15,
+				"visible": false
+			},
+			"children": elementList
+		}
+		var mainGroup = new UIGroup(data);
+		mainGroup.properties = data.properties;
+		this.elements.push(mainGroup);
+		this.elementLookupTable.push(mainGroup);
+		this.mainOptionDisplay = mainGroup;
+	}
 }
+
+Plume.prototype.processElementFromDefinition = function(elem) {
+	var newItem;
+	switch(elem.class) {
+		case "UIElement": 	newItem = new UIElement(elem.id); break;
+		case "UIString":	newItem = new UIString(elem.id); break;
+		case "UIRect":		newItem = new UIRect(elem.id); break;
+		case "UIImage":		newItem = new UIImage(elem.id); break;
+		case "UIGroup":		newItem = new UIGroup(elem); break;
+	}
+	newItem.properties = elem.properties;
+	newItem.events = this.processElementEvents(elem.events);
+	return newItem;
+}
+
+Plume.prototype.processElementEvents = function(events) {
+	var self = this;
+	var out = {};
+	for(var event in events) {
+		out[event] = typeof events[event] === "string" ? (function() { 
+			var eventName = events[event]; 
+			return function() { self.runScriptMethod(eventName); }
+		})() : events[event];
+	}
+	return out;
+}	
 
 Plume.prototype.parseConfig = function(data, processLine) {
 	var out = [];
@@ -359,9 +558,20 @@ Plume.prototype.start = function() {
 	//Set up keybinds
 	this.initializeKeyBindings();
 	
+	//Register events
+	
+	document.onclick = function(ev) {
+		var mx = ev.pageX;
+		var my = ev.pageY;
+		var elem = self.getElementAtPosition(mx, my);
+		if(elem && elem.events.onClick) elem.events.onClick(ev);
+	}
+
 	//Start the story!
 	this.displayNextDialogLine();
 	requestAnimationFrame(function() { self.draw() });
+	
+	
 }
 
 Plume.prototype.initializeKeyBindings = function() {
@@ -385,6 +595,7 @@ Plume.prototype.getConfigValue = function(key, def) {
 }
 
 Plume.prototype.displayNextDialogLine = function() {
+	var self = this;
 	if(this.activeBlock === undefined) {
 		this.error("The current block is not defined.");
 		return false;
@@ -401,15 +612,35 @@ Plume.prototype.displayNextDialogLine = function() {
 	
 	this.scrollSpeed = this.getSceneSettingsValue("scrollSpeed", 60);
 	
+	this.lookupElementById('main_option_display').properties.visible = false;
+	this.lookupElementById('main_text_display').properties.visible = true;
+	
 	var list = this.activeBlock.dialogList;
 	var line = list[this.blockLine++];
 	if(line instanceof DialogOption) {
 		//Display a dialog option
 		this.print(line.text);
-		for(var i = 0; i < line.options.length; i++) {
+		for(var i = 0; i < 4; i++) {
 			var option = line.options[i];
-			this.print("<button class='optionButton' onclick='javascript:Plume.goToBlock(\"" + option.block + "\")'>> " + option.text + "</button>");
+			if(option) {
+				var elem = this.lookupElementById('main_option_display_selection_' + i)
+				elem.properties.visible = true;
+				elem.setProperty("value", option.text);
+				elem.events.onClick = (function() {
+					var currOption = option;
+					return function() { 
+						self.goToBlock(currOption.block);
+					}
+				})();
+			} else {
+				this.lookupElementById('main_option_display_selection_' + i).properties.visible = false;
+
+			}
 		}
+		this.lookupElementById('main_option_display_dialog').setProperty("value", line.text);
+		this.lookupElementById('main_option_display').properties.visible = true;
+		this.lookupElementById('main_text_display').properties.visible = false;
+		
 	} else if(line instanceof ScriptCall) {
 		var func = line.func;
 		if(func.substr(func.length - 1, 1) === "!") {
@@ -424,21 +655,29 @@ Plume.prototype.displayNextDialogLine = function() {
 	} else {
 		this.currentLine = line.text;
 		this.currentLinePosition = 0;
-		this.mainDisplay.properties.value = line.character + ": ";
+		this.mainDisplay.setProperty("value", line.character + ": ");
 		this.writeTicks = 0;
 	}
+}
+
+Plume.prototype.lookupElementById = function(id) {
+	for(var i = 0; i < this.elementLookupTable.length; i++) {
+	    if(this.elementLookupTable[i].id === id) return this.elementLookupTable[i];
+	}
+	return null;
 }
 
 Plume.prototype.finishLine = function() {
 	//Skip the typewriter effect if there's still text to display, otherwise move on to the next line.
 	if(this.currentLinePosition < this.currentLine.length) {
 		while(this.currentLinePosition < this.currentLine.length) {
-			this.drawNextCharacter();
+			this.drawNextCharacter(true);
 		}
 	} else {
 		this.displayNextDialogLine();	
 	}
 }
+
 Plume.prototype.goToBlock = function(blockName) {
 	this.activeBlock = this.activeScene.blockList[blockName];
 	this.blockLine = 0;
@@ -449,6 +688,31 @@ Plume.prototype.goToBlock = function(blockName) {
 	this.displayNextDialogLine();
 }
 
+Plume.prototype.getElementAtPosition = function(x, y) {
+	var self = this;
+	var elems = this.elementLookupTable.slice(0);
+	elems = elems.sort(function(a, b) {
+		return self.getElementTotalZIndex(b) - self.getElementTotalZIndex(a);
+	})
+	for(var i = 0; i < elems.length; i++) {
+		var elem = elems[i];
+		if(elem.boundingBox && elem.properties.visible) {
+			var box = elem.boundingBox;
+			if(box.x1 <= x && box.x2 >= x && box.y1 <= y && box.y2 >= y && elem.hasEvents()) return elem;
+		}
+	}
+	return null;
+}
+
+Plume.prototype.getElementTotalZIndex = function(elem) {
+	var z = elem.properties.z;
+	var parent = elem.parent;
+	while(parent) {
+		z += parent.properties.z;
+		parent = parent.parent;
+	}
+	return z;
+}
 function builtinRead(x) {
     if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
             throw "File not found: '" + x + "'";
@@ -460,23 +724,23 @@ Plume.prototype.setupPython = function(script) {
 	Sk.configure({output: this.debug, read: builtinRead});
     try {
       this.activeScene.scriptModule = Sk.importMainWithBody("<stdin>", false, script); 
-    }
-    catch(e) {
-       alert(e.toString())
+    } catch(e) {
+       console.log(e.toString())
     }
 }
 
 Plume.prototype.runScriptMethod = function(method, args) {
 	if(args === undefined) {
-		Sk.misceval.callsim(this.activeScene.scriptModule.tp$getattr(method));	
+		//Have to use the instance or else the activeScene might not be set in the saved event environment
+		Sk.misceval.callsim(Plume.prototype.instance.activeScene.scriptModule.tp$getattr(method)); 	
 	} else {
-		args.unshift(this.activeScene.scriptModule.tp$getattr(method));
+		args.unshift(Plume.prototype.instance.activeScene.scriptModule.tp$getattr(method));
 		for(var i = 1; i < args.length; i++) args[i] = Sk.builtin.str(args[i]);
 		Sk.misceval["callsim"].apply(this, args);
 	}
 }
 
-Plume.prototype.processTag = function(line) {
+Plume.prototype.processTag = function(line, ignoreWait) {
 	var parts = line.replace("<", "").replace(">", "").split(":");
 	var key = parts[0].toLowerCase(), value = parts[1];
 	switch(key) {
@@ -501,19 +765,19 @@ Plume.prototype.processTag = function(line) {
 			this.scrollSpeed = parseInt(value, 10);
 			return "";
 		case "wait":
-			this.waitTime = parseInt(value, 10);
+			if(ignoreWait !== undefined && !ignoreWait) this.waitTime = parseInt(value, 10);
 	}
 	return line;
 }
 
-Plume.prototype.drawNextCharacter = function() {
+Plume.prototype.drawNextCharacter = function(ignoreWait) {
 	var nextChar = this.currentLine.substring(this.currentLinePosition, this.currentLinePosition + 1);
 	if(nextChar === "<" && this.currentLine.substring(this.currentLinePosition - 1, this.currentLinePosition) !== "\\") {
 		//Start of a tag.
 		var endTagPosition = this.currentLine.substring(this.currentLinePosition, this.currentLine.length).search(/[^\\]>/);
 		if(endTagPosition !== -1) {
 			endTagPosition += 2; //Right after the tag.
-			var tag = this.processTag(this.currentLine.substring(this.currentLinePosition, this.currentLinePosition + endTagPosition));
+			var tag = this.processTag(this.currentLine.substring(this.currentLinePosition, this.currentLinePosition + endTagPosition), ignoreWait);
 			if(tag !== "-skip-") {
 				this.mainDisplay.properties.value += tag;
 				this.currentLinePosition += endTagPosition;
@@ -531,11 +795,14 @@ Plume.prototype.draw = function() {
 
 	now = Date.now();
     delta = now - then;
+    
     this.writeTicks += delta;
     if(document.getElementById("fps")) document.getElementById("fps").innerHTML = Math.round(1000 / delta) + " FPS";
 	if (delta > interval) {
+		frameCount++;
 		then = now - (delta % interval);
 	    this.waitTime -= delta;
+	    if(frameCount % 15 === 0) this.recalculateBoundingBoxes();
 	    if(this.waitTime <= 0) {
 	    	this.waitTime = 0;
 			self.drawContext.clearRect(0, 0, self.canvas.width, self.canvas.height);
@@ -549,10 +816,32 @@ Plume.prototype.draw = function() {
 				this.writeTicks = 0;
 			}
 			for(var i = 0; i < this.elements.length; i++) {
-				if(this.elements[i].properties.visible) this.elements[i].draw(self.drawContext);
+				this.drawSpecificElement(self.drawContext, this.elements[i])
 			}
 		}
 	}
+}
+
+Plume.prototype.drawSpecificElement = function(context, element) {
+	if(element.properties.visible) {
+		element.draw(context);
+		if(element.properties.debug && element.boundingBox) {
+			var box = element.boundingBox;
+			context.setTransform(1, 0, 0, 1, 0, 0); //Reset transformations
+			context.beginPath();
+			context.rect(box.x1, box.y1, box.x2 - box.x1, box.y2 - box.y1);
+			context.stroke();
+			context.closePath();
+		}	
+	}
+}
+
+Plume.prototype.recalculateBoundingBoxes = function() {
+	var elems = this.elementLookupTable.slice(0);
+	elems.sort(function(a, b) {
+		return a.class === "UIGroup" ? 1 : -1
+	});
+	for(i = 0; i < elems.length; i++) elems[i].recalculateBoundingBox();
 }
 
 //FPS setters
@@ -561,6 +850,7 @@ var now;
 var then = Date.now();
 var interval = 1000/fps;
 var delta;
+var frameCount = 0;
 
 Plume.prototype.error = function(error, lineNumber, file) {
 	if(lineNumber !== undefined) {
@@ -587,6 +877,14 @@ Plume.prototype.print = function(line) {
 	if(document.getElementById('console')) 
 		document.getElementById('console').innerHTML = document.getElementById('console').innerHTML + line + "<br>";
 }
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
 
 var plume = new Plume();
 
